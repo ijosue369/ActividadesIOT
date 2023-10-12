b0f200d4fd45572368212a36adf6de5739dc6b16
Irving <irvingjosmx@gmail.com>
2023-10-11 18:01:21 -0600

    Actividad 1

---
+++ pg1_20231011.py
@@ -0,0 +1,12 @@
+from gpiozero import LED, Button
+from time import sleep
+
+ledR = LED(13)
+ledG = LED(14)
+ledB = LED(15)
+
+while True:
+    ledG.on()
+    sleep(0.5)
+    ledG.off()
+    sleep(0.5)
