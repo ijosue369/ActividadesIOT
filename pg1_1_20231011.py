939628edda910e9f1cc6e78043c389b8f0da804b
Irving <irvingjosmx@gmail.com>
2023-10-11 19:25:52 -0600

    Segundo Commit (Actividad 1_1)

--- pg1_1_20231011.py
+++ pg1_1_20231011.py
@@ -26,9 +26,9 @@ while True:
     color = input("Selecciona el color: ")
     parpadeo = input("¿Parpadeo? (0 = no, 1 = sí): ")
     DiccionarioColor = [x for x in listaColores if x[color] == color][0]
-    if parpadeo == 0:
+    if parpadeo == "0":
         LED(DiccionarioColor.values(1)).on()
-    elif parpadeo == 1:
+    elif parpadeo == "1":
         LED(DiccionarioColor.values(1)).on()
         sleep(0.5)
         LED(DiccionarioColor.values(1)).off()
