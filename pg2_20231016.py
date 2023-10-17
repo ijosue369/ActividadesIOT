4e40f78a3e7f4f6f547ea790b0bf11ec8c8d3a78
Irving <irvingjosmx@gmail.com>
2023-10-16 18:13:47 -0600

    Commit Actividad 2

---
+++ pg2_20231016.py
@@ -0,0 +1,38 @@
+'''
+Equipo:
+ - Aguirre Villanueva Juan Carlos
+ - López Terán Irving Josué
+ - Siles Asaff Juan Pablo
+'''
+#%%
+from gpiozero import LED
+from time import sleep
+import psutil 
+
+ledR = LED(16)
+ledG = LED(20)
+ledY = LED(21)
+
+while True:
+    cpuPorcentaje = int(psutil.cpu_percent(interval=1))
+    print(cpuPorcentaje)
+    if cpuPorcentaje <= 10:
+        ledR.off()
+        ledG.on()
+        ledY.off()
+    elif (cpuPorcentaje >= 10)&(cpuPorcentaje <= 20):
+        ledR.off()
+        ledG.off()
+        ledY.on()
+    elif cpuPorcentaje > 20:
+        ledG.off()
+        ledY.off()
+        ledR.on()
+        sleep(1)
+        ledR.off()
+        sleep(1)
+    else:
+        ledR.off()
+        ledG.off()
+        ledY.off()
+# %%
